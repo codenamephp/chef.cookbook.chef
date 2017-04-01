5b714634a31b0cadacea34cdb06efc89f8bdf0ab
Bastian Schwarz <bastian@codename-php.de>
2017-04-01 21:46:41 +0200

    Fixed typo

--- recipes/chef.rb
+++ recipes/chef.rb
@@ -2,7 +2,7 @@ package 'Install curl' do
   package_name 'curl'
 end
 
-execute 'Install chefdk with omintruck' do
+execute 'Install chefdk with omnitruck' do
   command 'curl -L https://omnitruck.chef.io/install.sh | sudo bash'
   notifies :delete, 'file[Delete omnitruck install script]', :before
   notifies :delete, 'file[Delete omnitruck install script]', :immediately
