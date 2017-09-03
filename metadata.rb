eb160680e79ddb90ae39820dc5c3809bb52982ad
Vagrant Default User <vagrant@stretch.localdomain>
2017-09-03 18:51:42 +0200

    Fixed default.rb to not include own cookbook name anymore

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,12 @@
 name 'chef.cookbook.cookbooks'
 maintainer 'Bastian Schwarz'
 maintainer_email 'basitan@codename-php.de'
-license 'Apache v2.0'
+license 'Apache-2.0'
 description 'Gets the environment ready to develop chef cookbooks by installing chefdk, vagrant, ruby, virtualbox, ...'
 long_description 'Gets the environment ready to develop chef cookbooks by installing chefdk, vagrant, ruby, virtualbox, ...'
-version '0.3.0'
+version '0.3.1'
+chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/codenamephp/chef.cookbook.cookbooks/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/codenamephp/chef.cookbook.cookbooks' if respond_to?(:source_url)
 
-depends 'apt'
-depends 'chef.cookbook.dev'
-
-supports 'debian', '~>8.1'
+supports 'debian', '~>9.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,9 @@
 #
-# Cookbook:: chef.cookbooks
+# Cookbook:: chef.cookbook.cookbooks
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe 'chef.cookbook.cookbooks::chef'
-include_recipe 'chef.cookbook.cookbooks::vagrant'
-include_recipe 'chef.cookbook.cookbooks::virtualbox'
+include_recipe '::chef'
+include_recipe '::vagrant'
+include_recipe '::virtualbox'
