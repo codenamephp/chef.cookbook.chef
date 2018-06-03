5f9697cb7ca79db1757f13bf34dc99a39607ba5f
Bastian Schwarz <bastian@codename-php.de>
2018-06-03 18:35:31 +0200

    Updated version for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'basitan@codename-php.de'
 license 'Apache-2.0'
 description 'Gets the environment ready to develop chef cookbooks by installing chefdk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.1.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/codenamephp/chef.cookbook.cookbooks/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/codenamephp/chef.cookbook.cookbooks' if respond_to?(:source_url)
