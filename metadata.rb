346c5f3ed828e633d95abe16ba5bd3673874e036
Bastian Schwarz <bastian@codenamephp.de>
2019-12-06 11:47:05 +0000

    Fixes #9 Updated travis build

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 name 'codenamephp_chef'
 maintainer 'Bastian Schwarz'
 maintainer_email 'basitan@codename-php.de'
 license 'Apache-2.0'
 description 'Gets the environment ready to develop chef cookbooks by installing chefdk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+version '2.0.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/codenamephp/chef.cookbook.cookbooks/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/codenamephp/chef.cookbook.cookbooks' if respond_to?(:source_url)
 
-supports 'debian', '~>9.1'
+supports 'debian', '~>10.0'
 
 depends 'line', '~>2.0.0'
