4d304553f4e3d95409c50446a8244384246983b6
Bastian Schwarz <bastian@codename-php.de>
2020-06-24 21:34:33 +0200

    #17 also cleared smoke tests

--- test/smoke/default/chef_spec.rb
+++ test/smoke/default/chef_spec.rb
@@ -8,37 +8,4 @@
 control 'chef-1.0' do
   title 'Install chefdk'
   desc 'Install chefdk, remove the install script and init the shell'
-
-  describe package('chefdk') do
-    it { should be_installed }
-  end
-
-  describe command('chef -v') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/ChefDK version/) }
-  end
-
-  describe command('kitchen -v') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/Test Kitchen/) }
-  end
-
-  describe file('/tmp/install.sh') do
-    it { should_not exist }
-  end
-
-  describe file('/etc/bash.bashrc') do
-    it { should exist }
-    its('content') { should match(Regexp.new(Regexp.quote('eval "$(chef shell-init bash)"'))) }
-  end
-
-  describe file('/etc/bash.bashrc') do
-    it { should exist }
-    its('content') { should match(Regexp.new(Regexp.quote('export USE_DOKKEN=true'))) }
-  end
-
-  describe file('/etc/bash.bashrc') do
-    it { should exist }
-    its('content') { should match(Regexp.new(Regexp.quote('export KITCHEN_LOCAL_YAML=.kitchen.dokken.yml'))) }
-  end
 end
